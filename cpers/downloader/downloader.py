fe79153347390dd3943156b95484ea00340e1899
Asepimam <asepimam329@gmail.com>
2025-12-24 13:25:35 +0700

    refactor Downloader class to include constructor with options and download path

--- cpers/downloader/downloader.py
+++ cpers/downloader/downloader.py
@@ -1,3 +1,9 @@
+from abc import ABC, abstractmethod
+
 class Downloader:
-    def __init__(self):
+    def __init__(self, ydl_opts:object, download_path:str ):
+        self.ydl_opts = ydl_opts 
+        self.download_path = download_path
+    @abstractmethod(ABC)    
+    def download(self, url:str, donwload_path:str):
         pass
